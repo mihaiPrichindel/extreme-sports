5f1234c5b9235c034fa7049337be4223bc041f45
Prichindel Mihai <mihai.prichindel@gmail.com>
2019-11-02 15:47:40 +0200

    create entities

--- src/main/java/com/sda/hibernate_utils/HibernateUtil.java
+++ src/main/java/com/sda/hibernate_utils/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.sda.hibernate_utils;
 
+import com.sda.entity.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -23,9 +24,13 @@ public class HibernateUtil {
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+         //       settings.put(Environment.HBM2DDL_AUTO, "create-drop");
                 configuration.setProperties(settings);
-            //    configuration.addAnnotatedClass(Student.class);
+                configuration.addAnnotatedClass(City.class);
+                configuration.addAnnotatedClass(Country.class);
+                configuration.addAnnotatedClass(Place.class);
+                configuration.addAnnotatedClass(Region.class);
+                configuration.addAnnotatedClass(Sport.class);
                 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties()).build();
                 sessionFactory = configuration.buildSessionFactory(serviceRegistry);
